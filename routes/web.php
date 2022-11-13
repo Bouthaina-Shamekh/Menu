ee18fb2d253aaa8df6e18449c7afc8f66070f173
unknown <buthayna.shamekh@gmail.com>
2022-11-13 15:47:53 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\QrController;
+use App\Http\Controllers\Admin\MealController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +21,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::resource('categories', CategoryController::class);
+    Route::resource('meals', MealController::class);
+    Route::resource('menus',  MenuController::class);
+    Route::resource('qr',  QrController::class);
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
+    Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 });
